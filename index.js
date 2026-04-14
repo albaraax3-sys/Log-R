16200113f2af3d2a5879c97c0b249fc268633fac
albaraax3-sys <albaraa.x3@gmail.com>
2026-04-14 13:41:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
-
 const app = express();
-app.get('/', (req, res) => res.send('Bot is online!'));
-app.listen(3000);
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+app.get('/', (req, res) => {
+  res.send('Bot is Online!');
 });
+
+app.listen(3000, () => {
+  console.log('Server is ready!');
+});
+
+// --- هنا تضع كود بوت الديسكورد الخاص بك (discord.js) ---
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
-    console.log('Logged in as ' + client.user.tag);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (message) => {
-    if (message.content === '!ping') {
-        message.reply('Pong!');
-    }
-});
-
-client.login(process.env.TOKEN)
+client.login('ضع_التوكن_هنا');
